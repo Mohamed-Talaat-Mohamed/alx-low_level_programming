3e7bda7d7d356fb0a15f86e7d88a029d454e0bfd
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-15 06:04:36 -0700

    adding 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Entry Point
+*Description: This is a C program to print with printf function
+*Return: Always 0 (pass)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
