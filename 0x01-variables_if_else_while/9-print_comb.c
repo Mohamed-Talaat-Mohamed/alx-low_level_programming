2ceed169f738ea9fe57084ff18e8c433b13a15b2
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-16 11:34:53 -0700

    Uploading 9-print_comb.c Answer

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *Description: A C program printsf *possible combinations of single-digit numbers
+ *
+ *Return: Always 0 (Success)
+*/
+
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	if (d != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
