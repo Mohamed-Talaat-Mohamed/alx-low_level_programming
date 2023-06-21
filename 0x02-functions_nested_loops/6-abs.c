fc51a8c1c71ef386c615f31a00df7fb97f00e9bf
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-21 06:51:58 -0700

    Uploading

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_abs - Computes the absolute value of an integer
+ *@c: The number to be computed
+ *Return: Absolute value of number or zeri
+*/
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abc_val;
+
+		abc_val = c * -1;
+		return (abc_val);
+	}
+	return (c);
+
+}
