447cafcc038af0fb562321bb2cf0837830341ae9
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-21 06:06:35 -0700

    hotfix_3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,7 @@
 /**
  * _islower - Checks for lowercase character
  *&c: The character to be checked
- * Return: 1 for lowercase character
- *	   0 for any*
+ * Return: 1 if c lowercase other always 0
 */
 
 int _islower(int c)
