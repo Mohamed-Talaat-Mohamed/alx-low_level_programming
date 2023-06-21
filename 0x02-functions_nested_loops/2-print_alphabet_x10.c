9049482b1ae0555c8a47031dd69603061e8a861b
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-21 05:42:42 -0700

    hotfix_1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * void - Entry Point
- *Descreption: print alphabet x10 times
- * Return: void
+ *print_alphabet_x10 - Make alphabet x10 times
+ *Return: void
 */
 
 void print_alphabet_x10(void)
