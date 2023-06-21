37dc8f52171254654d6d2c6d9d8c6cc7cfc87f60
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-21 06:27:56 -0700

    hotfix_1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _isalpha - Checks for alphabetic character
- *@c: The character to be checked
- * Return: 1 for alphabetic character or 0 for anything else
-*/
-
+ * _isalpha - check if character is lowercase
+ *@c: Checks if the character 'c' is lowercase
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
