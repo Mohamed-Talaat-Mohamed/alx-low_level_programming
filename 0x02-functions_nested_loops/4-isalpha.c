267329e442f461e5fe7362bc587d73241f088576
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-21 06:40:46 -0700

    hotfix_2

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,15 +4,13 @@
  * _isalpha - check if character is lowercase
  *@c: Checks if the character 'c' is lowercase
  * Return: 1 if c is lowercase, 0 otherwise
- */
+*/
+
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
