0079696423254e5274e9a322c24f7bf0204e6544
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-26 06:02:00 -0700

    hotfix 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string
+ * _puts - Write a function that prints a string, followed by a new line
  *
- * @str: The string to print
+ * @str: The string
  *
  * Return: Nothing
 */
+
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	while (*str != '\0')
 	{
-		_putchar(*str);
+		_putchar(*str + 0);
+		str++;
 	}
 
 	_putchar('\n');
