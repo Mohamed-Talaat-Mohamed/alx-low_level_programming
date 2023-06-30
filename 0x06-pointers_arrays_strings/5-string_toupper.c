99fd803607cb4dc86c1af5ae9a84e6fc6f6694f1
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-06-29 17:41:37 -0700

    hotfix2

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,16 +9,16 @@
  * Return: (string)
 */
 
-char *string_toupper(char *s)
+char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			s[i] = s[i] - 32;
+			str[i] = str[i] - 32;
 		}
 	}
-	return (s);
+	return (str);
 }
