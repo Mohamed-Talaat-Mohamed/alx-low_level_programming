827ce03bccaf818dd5ccc32cc1c0e193de1f3f4f
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-07-03 07:01:42 -0700

    Uploading 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset -  fills memory with a constant byte.
+ *
+ * @s: string
+ *
+ * @b: const byte
+ *
+ * @n: memory area pointed to by s
+ *
+ * Return: a pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
