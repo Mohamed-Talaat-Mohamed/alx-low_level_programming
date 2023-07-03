e424ac29546a027c9f818f596f8fbb4beb061533
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-07-03 07:16:49 -0700

    uploading

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strspn - gets the length of a prefix substring.
+ *
+ * @s: string
+ *
+ * @accept: consist
+ *
+ * Return: the number of bytes in the initial segment of
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, r;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (r = 0; accept[r] != s[i]; r++)
+		{
+			if (accept[r] == '\0')
+				return (i);
+		}
+	}
+	return (i);
+}
