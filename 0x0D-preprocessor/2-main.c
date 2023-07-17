cc29155c48d95640a9b015052e0b29d3aed29ace
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-07-17 06:34:53 -0700

    hotfix6

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - print the name of this source file
+ *main - main Entry
+ *
+ *print the name of this source file
  *
  *Return: Always 0
- */
+*/
 
 int main(void)
 {
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,5 +2,5 @@
 #define _FUNCTION_LIKE_MACRO_H
 
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < 0 ? (-(x) : (x)))
 #endif
