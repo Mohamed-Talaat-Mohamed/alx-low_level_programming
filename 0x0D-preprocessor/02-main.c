c16d614fd99bfe7561ef6856fdb2f34af52ed2f5
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-07-17 06:11:58 -0700

    hotfix 3

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
-  *main - print the name of this source file
-  *Return: Always 0
-  */
+ *main - print the name of this source file
+ *
+ *Return: Always 0
+ */
+
 int main(void)
 {
 	printf("%s \n", __FILE__);
-	return 0;
+	return (0);
 }
