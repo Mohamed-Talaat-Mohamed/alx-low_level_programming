98834af67386a1e10ea001a189d4e69a18ea5c07
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-07-19 17:36:38 -0700

    f

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,33 +1,25 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - function pointer that selects the correct function to perform
- * the operation asked by the user
- * @s: the operator given by the user
- * Return: pointer to the function that corresponds to the
- * operator given as a parameter
- */
-
+ * get_op_func - fetching functions to be performed
+ * @s: operator
+ * Return: a pointer to the function of the operator given
+*/
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
-		{"*", op_mul},
 		{"/", op_div},
+		{"*", op_mul},
 		{"%", op_mod},
-		{NULL, NULL}
+		{NULL, NULL},
 	};
-	int i;
 
-	i = 0;
+	int i = 0;
 
-	while (ops[i].op)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
-	return (NULL);
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,16 @@
 #include "3-calc.h"
-
+#include "function_pointers.h"
 /**
- * main - program that perfroms simple operations
+ * main - Prints the result of operations
  * @argc: number of arguments
- * @argv: array of arguments
- * Return: Always 0 (Success)
+ * @argv: array of pointers
+ *
+ * Return: Always 0 success
  */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+	int n1, n2;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -19,27 +18,25 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+	n1 = atoi(argv[1]);
+	op = argv[2];
+	n2 = atoi(argv[3]);
 
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	if ((*op == '/' && n2 == 0) ||
+	    (*op == '%' && n2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = func(arg1, arg2);
+	printf("%d\n", get_op_func(op)(n1, n2));
 
-	printf("%d\n", result);
+	return (0);
+}
 
-	return (0);}
