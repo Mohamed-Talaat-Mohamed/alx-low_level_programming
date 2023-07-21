b628e752cd2adc8bb9a34b37244a0914b2dff116
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-07-20 18:56:09 -0700

    uploading the directories and folders

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: arguments
+ * @...: variable number of arguments to calculate it’s sum
+ * Return: 0 if n == 0
+ *	Otherwise - the sum of all parameters.
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+
+	va_start(ap, n);
+
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+
+	va_end(ap);
+
+	return (sum);
+}
