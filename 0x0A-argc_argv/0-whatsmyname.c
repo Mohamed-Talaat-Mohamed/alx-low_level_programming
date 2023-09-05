f1e67009b8da5a340194c0da942246f44acca183
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-05 02:39:53 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,17 @@
 
 /**
  * main - Entry point
- *A program that print it`s name followed by new line
+ * A program that print it`s name followed by new line
  * @argc: Number of arguments
  * @argv: Array of arguments
  * return: Always 0 (success)
- */
+*/
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
