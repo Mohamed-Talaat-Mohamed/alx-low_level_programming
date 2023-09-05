64a5adc2e4d646ff6fbd9da1eb20562713938fb2
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-05 00:46:25 -0700

    0 task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- * @argc: a length of argv array
- * @argv: an array of argc
- * return: (0)
+ * main - A function to print the program name
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	
-	printf("%s\n", argv[0]);
+
+	printf("%s\n", *argv);
 
 	return (0);
 }
