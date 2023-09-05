d16d83707c97741aacbe06ce45feaf2498d990ae
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-05 02:17:45 -0700

    up

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * A program that multiplies two numbers
+ * @argc: Numbers of arguments
+ * @argv: Array of arguments
+ * Return: if doesn't recieve 2 arguments return 1
+ *	   if recieved 2 arguments return 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	int sum = atoi(argv[1]) * atoi(argv[2]);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	return (1);
+	}
+	else
+	{
+		printf("%d\n", sum);
+	}
+	return (0);
+}
