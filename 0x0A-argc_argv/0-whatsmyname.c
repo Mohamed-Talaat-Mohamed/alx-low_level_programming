4699614e1e76e9978ea07decd8f47692a539af9a
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-06 04:12:30 -0700

    refix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point
- * A program that print it`s name followed by new line
+ * a program that print its name followed by new line
  * @argc: Number of arguments
  * @argv: Array of arguments
- * return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(int argc, char *argv[])
