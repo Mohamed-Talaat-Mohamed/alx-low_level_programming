a2fea087fbb606cb153cd773ce167073b3d606e9
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-12 04:16:06 -0700

    all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
  * struct dog - a new type struct
  *
@@ -18,11 +19,14 @@ struct dog
 	char *owner;
 };
 
+
 /**
  * dog_t - a new type for struct dog
- */
+*/
 
 typedef struct dog dog_t;
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
