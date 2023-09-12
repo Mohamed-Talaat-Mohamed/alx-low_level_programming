9e5b8e23b2df7a2df7358c0a23ed7b521fe1239a
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-12 02:05:17 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type struct
+ *
+ * @name:first element
+ * @age: seconf element
+ * @owner: third element
+ *
+ * Description: a new type struct for a dog's information
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
