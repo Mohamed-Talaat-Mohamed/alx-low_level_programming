dbc41ae8a092ec481174b662966fc4e45e1b6b51
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-12 06:01:44 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * *_memset - copies const char to memory
+ * @s: memory to be reserved
+ * @b: char
+ * @n: times of copying b
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+
+	return (s);
+}
+
+/**
  * _calloc - function that allocates memory for an array, using malloc.
  * @nmemb: number of elements
  * @size: size of each elements
