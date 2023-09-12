daa8e2a06b264ca25557da61693ab197eaf3010c
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-12 03:50:31 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 	{
-		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+		printf("Owner: %s\n", d->owner);
 	}
 	
 }
