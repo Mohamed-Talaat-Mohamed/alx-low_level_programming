22415375c1479cdb9a8d19cf70f8e8d937bade16
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-12 02:24:18 -0700

    a

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -9,9 +11,9 @@
  * Return: void
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
