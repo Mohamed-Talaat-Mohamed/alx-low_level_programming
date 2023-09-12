fa35d64b8ce9fa75decd82161b112483e4867241
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-12 05:35:02 -0700

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * new_dog - creates a new dog
+ * @name: name member of the dog
+ * @age: age member of the dog
+ * @owner: owner member of the dog
+ *
+ * Return: pointer to the new dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	return NULL;
+
+	newDog->name = (char *)malloc(strlen(name) + 1);
+	newDog->owner = (char *)malloc(strlen(owner) + 1);
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return NULL;
+	}
+	strcpy(newDog->name, name);
+	newDog->age = age;
+	strcpy(newDog->owner, owner);
+	return newDog;
+}
