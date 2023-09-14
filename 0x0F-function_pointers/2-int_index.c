a6f13f36f7952ff7438ea0395ab7929bc76f4402
mohamed-talaat-mohamed <mohamed.talaat@flo-egypt.com>
2023-09-14 00:55:16 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,15 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int x;
+
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 
-	int x;
-
 	for (x = 0; x < size ; x++)
 	{
-		if (cmp(array[x]))
-			return (x);
+	if (cmp(array[x]))
+		return (x);
 	}
 	return (-1);
 }
