39efdb1bb9341ade8f0013573bc875efa5223a76
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-09-26 05:28:46 -0700

    hotfix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,13 +12,15 @@ size_t print_listint(const listint_t *h)
 {
 	size_t x = 0;
 
-	do {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
+		
 		x++;
 		
 		h = h->next;
 
-	} while (h != NULL);
+	}
 
 	return (x);
 }
