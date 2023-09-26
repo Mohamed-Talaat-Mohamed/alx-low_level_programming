a3722efad1ac91963da27bf623026cbdc983763d
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-09-26 05:18:12 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,20 @@
-qmfpqepfmpomfpompoemfpo	mqp
+#include "lists.h"
+
+/**
+ * listint_len - function to print linked list number of elements
+ *
+ * @h: pointer of first node
+ *
+ * Return: size of list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t x = 0;
+
+	do {
+		h = h->next;
+		x++;
+	} while (h);
+	return (x);
+}
