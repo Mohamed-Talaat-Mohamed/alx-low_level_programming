156910a34c2d670530ce55b2e15b6d11cbe4b695
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-09-28 18:23:50 -0700

    up

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,10 +14,12 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
+
 	for (x = 0; b[x]; x++)
 	{
-	if (b[x] < '0' || b[x] > '1')
+	if (b[x] != '0' || b[x] != '1')
 		return (0);
+
 	num = (num * 2) + (b[x] - '0');
 	}
 	return (num);
