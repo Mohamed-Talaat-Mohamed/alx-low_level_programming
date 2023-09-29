ca6d9a283e5d9bc1e93721a6b6e189fbfa893686
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-09-28 18:21:03 -0700

    up

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
+ * binary_to_uint - function to convert binary num to unsigned int
  *
  * @b: list of ones and zeros
  *
- * Return: num or zero if faild
+ * Return: num if success or zero if faild
  */
 
 unsigned int binary_to_uint(const char *b)
