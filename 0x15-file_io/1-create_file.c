4acca4b28986b34e7800a59014aaa2876293ee01
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-10-04 18:19:39 -0700

    uploade task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file -  a function that creates a file.
+ *
+ * @filename : file name
+ *
+ * @text_content : tect content
+ *
+ * Return: 1 success , -1 if fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	int x = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+
+	if (fp == -1)
+		return (-1);
+
+	else if (text_content != NULL)
+		x = strlen(text_content);
+
+	else if (write(fp, text_content, x) != -1)
+		return (1);
+
+	else
+		return (-1);
+
+	close(fp);
+
+	return (1);
+}
