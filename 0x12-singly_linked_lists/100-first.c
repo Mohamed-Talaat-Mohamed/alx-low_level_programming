112efe67f0cfd7b008fa7a387ff6abaa2fae3bbd
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-10-10 07:22:45 -0700

    update task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@ void before(void)__attribute__((constructor));
  * before - a function that prints custom message
  *	before the main function is executed
 */
-void before()
+void before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
