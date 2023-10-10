c03de88762705a18e837dbcafde06cb127d1925b
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-10-10 07:44:49 -0700

    re load

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,9 +3,9 @@
 /**
  * print_listint - a function to prints all the elements of a listint_t
  *
- * @h: pointer to the list
+ * @h: pointer
  *
- * Return: the number of nodes
+ * Return: numbers of node
  */
 
 size_t print_listint(const listint_t *h)
