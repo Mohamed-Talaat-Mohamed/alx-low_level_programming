4d6cd3130c2bfa4ec48f1d59051dcd542f1a1864
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-10-11 02:36:34 -0700

    up

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void close_elf(int elf);
+void check_elf(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+
+/**
+ * check_elf - a function to Check the file is ELF or not
+ * @e_ident: pointer to ELF`s array
+ */
+
+void check_elf(unsigned char *e_ident)
+{
+	int indx;
+
+	for (indx = 0; indx < 4; indx++)
+	{
+		if (e_ident[indx] != 127 &&
+		    e_ident[indx] != 'E' &&
+		    e_ident[indx] != 'L' &&
+		    e_ident[indx] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+
+/**
+ * print_class - a function to Print classic ELF header file
+ * @e_ident: pointer to ELF`s array
+ */
+
+void print_class(unsigned char *e_ident)
+{
+	printf("  Class:                             ");
+
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
+/**
+ * print_magic - a function to Print magic ELF header file 
+ * @e_ident: pointer to ELF`s array 
+ */
+
+void print_magic(unsigned char *e_ident)
+{
+	int indx;
+
+	printf("  Magic:   ");
+
+	for (indx = 0; indx < EI_NIDENT; indx++)
+	{
+		printf("%02x", e_ident[indx]);
+
+		if (indx == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
+
+/**
+ * print_version - a function to Print ELF header file verion
+ * @e_ident: pointer to ELF`s array
+ */
+
+void print_version(unsigned char *e_ident)
+{
+	printf("  Version:       %d",
+	       e_ident[EI_VERSION]);
+
+	switch (e_ident[EI_VERSION])
+	{
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
+	}
+}
